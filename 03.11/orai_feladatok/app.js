9433b4bf6a1f9220a863c2d5df5f7ea5c7e55d05
bernathmark <nitro_5_bernath_mark@outlook.hu>
2023-03-11 15:01:42 +0100

    update

--- 03.11/orai_feladatok/app.js
+++ 03.11/orai_feladatok/app.js
@@ -4,34 +4,46 @@ function teglaTestTerfogatEsFelszinKalkulator(a, b, c) {
       a * b * c +
       "<br>" +
       "A teglatest felszine: " +
-      2 * (a * b + a * c + b * c) + '<br>'
+      2 * (a * b + a * c + b * c) +
+      "<br>"
   );
 }
 
 teglaTestTerfogatEsFelszinKalkulator(1, 2, 3);
 
-
 function lottoSzamGenerator(alsoHatar, felsoHatar, kihuzandoSzamokSzama) {
-    let lottoSzamok = [];
-  
+  let lottoSzamok = [];
 
-    while (lottoSzamok.length < kihuzandoSzamokSzama) {
-      let randomSzamok = Math.round(Math.random() * (felsoHatar-alsoHatar)) + alsoHatar;
-  
+  while (lottoSzamok.length < kihuzandoSzamokSzama) {
+    let randomSzamok =
+      Math.round(Math.random() * (felsoHatar - alsoHatar)) + alsoHatar;
 
-      if (lottoSzamok.indexOf(randomSzamok) === -1) {
-        lottoSzamok.push(randomSzamok);
-      }
+    if (lottoSzamok.indexOf(randomSzamok) === -1) {
+      lottoSzamok.push(randomSzamok);
     }
-  
-
-    return lottoSzamok;
   }
-  
-document.write(lottoSzamGenerator(1,80,10)+ '<br>')
 
-function szazalekAtvalto(ertek, szazalek){
-    return ujErtek = ertek/100*szazalek
+  return lottoSzamok;
 }
 
-document.write(szazalekAtvalto(3,50))
+document.write(lottoSzamGenerator(1, 80, 10) + "<br>");
+
+function szazalekAtvalto(ertek, szazalek) {
+  return (ujErtek = (ertek / 100) * szazalek);
+}
+
+document.write(szazalekAtvalto(3, 50) + "<br>");
+
+//szam atlag szamitas
+
+function atlagSzamitas(szamokSzama) {
+  let osszeg = 0;
+  let atlag = 0;
+  for (let i = 1; i <=szamokSzama; i++) {
+    osszeg = osszeg + i;
+  }
+  return (atlag = osszeg / szamokSzama);
+}
+
+document.write(atlagSzamitas(1000000) + "<br>");
+
